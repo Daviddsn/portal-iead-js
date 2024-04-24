f1e9f98b2be959f0ee1ea4b7040726caf6de90eb
Davi Santos <davisdn@dcompu.ufs.br>
2024-04-24 16:48:48 -0300

    update servidor

--- portal-front/src/server.js
+++ portal-front/src/server.js
@@ -4,13 +4,13 @@ import nunjucks from 'nunjucks';
 import { routes } from "./router.js";
 import fs from 'node:fs'
 
-import {menuData} from './shared.js';
+import { menuData } from './shared.js';
 
 
 
 const server = express()
 
-const env  = nunjucks.configure('./src/views', {
+const env = nunjucks.configure('./src/views', {
     express: server,
     autoescape: true,
     noCache: true,
@@ -18,23 +18,25 @@ const env  = nunjucks.configure('./src/views', {
 
 env.addGlobal('menuData', menuData)
 
+server.set('view engine', 'html')
+
 
 
 // Inicio e configuração do servidor
 server
-// receber os dados do req.body
-.use(express.urlencoded({ extended: true }))
-// configurar arquivos estáticos (css, scripts, imagens)
-.use(express.static("./public"))
+    // receber os dados do req.body
+    .use(express.urlencoded({ extended: true }))
+    // configurar arquivos estáticos (css, scripts, imagens)
+    .use(express.static("./public"))
 
-.use(routes);
+    .use(routes);
 
 
 const porta = process.env.PORT_APP || 4000
 
 
 
-server.listen(porta, () => console.log( "Servidor Rodando na porta " + porta))
+server.listen(porta, () => console.log("Servidor Rodando na porta " + porta))
 
 
 // https.createServer(credentials , server).listen(porta, () => console.log( "Servidor Rodando na porta " + porta))
